5c1f02080884c5b08eb6e3bdbae452b56ca12f84
Tiagothyga <102805728+Tiagothyga@users.noreply.github.com>
2022-04-16 03:56:23 -0300

    Add files via upload

---
+++ Atividade.py
@@ -0,0 +1,10 @@
+x = input("Insira seu nome ")
+
+y = input("Insira seu salario")
+
+parte1 = y[0:1]
+parte2 = y[1:4]
+
+s = ("O salário de " + x + " no mês de Abril foi de R$" + parte1 + "." + parte2 + ",00 reais")
+
+print (s)
